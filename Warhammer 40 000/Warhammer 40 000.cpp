15cbdc2d62e378fc441ae521c847af8846b232dc
Quark <gennadiy.kaydalov@mail.ru>
2021-07-31 22:29:19 +0300

    На данный момент я сильно оптимизировал дерево и оно начинает догонять брутфорс.Однако всё-равно уступает ему

--- Warhammer 40 000/Warhammer 40 000.cpp
+++ Warhammer 40 000/Warhammer 40 000.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <random>
 
+
 using namespace std;
 
 struct FVector
@@ -13,14 +14,11 @@ struct FVector
     float X;
     float Y;
 
-    /// <summary>
-    /// Get lenght of vector
-    /// </summary>
-    /// <returns></returns>
     float magnitude() {
         return sqrt((X * X) + (Y * Y));
     }
-};// Назвал так-же,как в UE4
+};//Назвал так-же,как в UE4
+
 class Unit {
 public:
     FVector Position;
@@ -38,14 +36,136 @@ public:
         DistanceView = distanceView;
     }
 };
-
 vector<Unit> unit;
 
-/// <summary>
-/// get size of file
-/// </summary>
-/// <param name="filename"></param>
-/// <returns></returns>
+class Rectangle {
+public:
+    float X;
+    float Y;
+    float W;
+    float H;
+    Rectangle(float x = 0, float y = 0, float w = 0, float h = 0) {
+        X = x;
+        Y = y;
+        W = w;
+        H = h;
+    }
+
+    bool Contains(Unit unit) {
+        return (
+            unit.Position.X >= X - W &&
+            unit.Position.X < X + W &&
+            unit.Position.Y >= Y - H &&
+            unit.Position.Y < Y + H
+            );
+    }
+
+    bool Intersects(Rectangle range) {
+        return !(
+            range.X - range.W > X + W ||
+            range.X + range.W <X - W ||
+            range.Y - range.H > Y + H ||
+            range.Y + range.H < Y - H
+            );
+    }
+};
+static vector<int> s;
+class QuadTree {
+    int Capacity;
+
+public:
+    Rectangle Boundary;
+    vector<Unit> Points;
+    bool Divided;
+
+    QuadTree* NorthWest;
+    QuadTree* NorthEast;
+    QuadTree* SouthWest;
+    QuadTree* SouthEast;
+
+    QuadTree(Rectangle boundary, int capacity) {
+        Boundary = boundary;
+        Capacity = capacity;
+        Divided = false;
+    }
+
+    void subdivide() {
+        auto x = Boundary.X;
+        auto y = Boundary.Y;
+        auto w = Boundary.W;
+        auto h = Boundary.H;
+        auto ne = Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+        NorthEast = new QuadTree(ne, Capacity);
+        auto nw = Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+        NorthWest = new QuadTree(nw, Capacity);
+        auto se = Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+        SouthEast = new QuadTree(se, Capacity);
+        auto sw = Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+        SouthWest = new QuadTree(sw, Capacity);
+        Divided = true;
+    }
+
+    bool insert(Unit unit) {
+        if (!Boundary.Contains(unit)) {
+            return false;
+        }
+
+        if (Points.size() < Capacity) {
+            Points.push_back(unit);
+            return true;
+        }
+        else {
+            if (NorthWest == nullptr) {
+                subdivide();
+            }
+            if (NorthWest->insert(unit)) {
+                return true;
+            }
+            else if (NorthEast->insert(unit)) {
+                return true;
+            }
+            else if (SouthWest->insert(unit)) {
+                return true;
+            }
+            else if (SouthEast->insert(unit)) {
+                return true;
+            }
+        }
+    }
+
+    void query(Rectangle range, vector<Unit>& found) {
+        if (!Boundary.Intersects(range)) {
+            return;
+        }
+        else {
+            for (size_t i = 0; i < Points.size(); i++) {
+                if (range.Contains(Points[i])) {
+                    found.push_back(Points[i]);
+                }
+            }
+            if (NorthWest == nullptr) {
+                return;
+            }
+            if (Divided) {
+                NorthWest->query(range, found);
+                NorthEast->query(range, found);
+                SouthWest->query(range, found);
+                SouthEast->query(range, found);
+            }
+        }
+        return;
+    }
+};
+
+Rectangle boundry = Rectangle(0, 0, 10, 10);
+QuadTree quadTree = QuadTree(boundry, 20);
+
+void Setup() {
+    for (size_t i = 0; i < unit.size(); i++)
+    {
+        quadTree.insert(unit[i]);
+    }
+}
 std::ifstream::pos_type filesize(const char* filename)
 {
     std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
@@ -54,7 +174,6 @@ std::ifstream::pos_type filesize(const char* filename)
 void SaveDataUnits() {
     for (size_t i = 0; i < unit.size(); i++)
     {
-
         string info =
             to_string(unit[i].Position.X)
             + "\n"
@@ -87,7 +206,7 @@ void LoadDataUnits() {
     float AngleView;
     float Distance;
 
-    std::ifstream in("InfoAboutUnits.txt"); 
+    std::ifstream in("InfoAboutUnits.txt");
     if (in.is_open())
     {
         while (in >> PosX >> PosY >> DirViewX >> DirViewY >> AngleView >> Distance)
@@ -100,23 +219,17 @@ void LoadDataUnits() {
         }
     }
 }
-/// <summary>
-/// Checks if the target is within the unit's scope.
-/// </summary>
-/// <param name="unit"></param>
-/// <param name="targetPos"></param>
-/// <returns></returns>
 bool CheckView(Unit unit, FVector targetPos) {
     FVector Vector =
     { targetPos.X - unit.Position.X,
-      targetPos.Y - unit.Position.Y };// Computing a vector to another unit
+      targetPos.Y - unit.Position.Y };//Вычисление вектора до другого юнита
 
-    if (Vector.magnitude() <= unit.DistanceView) { // Вычисление дистанции до цели ради преждевременного отсечения целей,которые выходят за пределы видимости
+    if (Vector.magnitude() <= unit.DistanceView) { //Вычисление дистанции до цели ради преждевременного отсечения целей,которые выходят за пределы видимости
 #pragma region Calculacte angle between 2 vectors
         float dot = (unit.DirectionView.X * Vector.X) + (unit.DirectionView.Y * Vector.Y);
         float det = (unit.DirectionView.X * Vector.Y) - (unit.DirectionView.Y * Vector.X);
 
-        float angle = atan2(det, dot) * (180.0 / 3.141592653589793238463);// Calculating the angle between vectors in radians and then converting to degrees
+        float angle = atan2(det, dot) * (180.0 / 3.141592653589793238463);//вычисление угла между векторами в радианах и последующяя конвертация в градусы
 #pragma endregion
         if (angle < unit.AngleView / 2) {
             return true;
@@ -128,24 +241,25 @@ bool CheckView(Unit unit, FVector targetPos) {
     }
     return false;
 }
-/// <summary>
-/// Отвечает за проверку видимости других юнитиов для каждого юнита
-/// </summary>
-/// <param name="start"></param>
-/// <param name="end"></param>
-void Controller(int start,int end) {
+void Controller(int start, int end) {
+    int CountSeeUnit = 0;
     for (size_t i = start; i < end; i++)
     {
-        int CountSeeUnit = 0;
-        for (size_t j = 0; j < unit.size(); j++)
+        Rectangle range = Rectangle(unit[i].Position.X, unit[i].Position.Y, 5, 5);
+
+        vector<Unit> unitInRangeView;
+        quadTree.query(range, unitInRangeView);//Все юниты,которые находятся в радиусе обзора юнита
+
+        CountSeeUnit = 0;
+        for (size_t j = 0; j < unitInRangeView.size(); j++)
         {
-            if (CheckView(unit[i], unit[j].Position)) {
-                CountSeeUnit++;
-            }
+            //if (CheckView(unit[i], unitInRangeView[j].Position)) {
+                //CountSeeUnit++;
+            //}
         }
-        printf("Unit Number %i sees %i unit(s) \n", i, CountSeeUnit);
+        //printf("Unit Number %i sees %i unit(s) \n", i, CountSeeUnit);
     }
-}
+} //Адская машина,которая отвечает за проверку видимости других юнитиов для каждого юнита
 void AddUnit()
 {
 #pragma region Fucking Rand in C++
@@ -160,10 +274,10 @@ void AddUnit()
     for (size_t i = 0; i < 10000; i++)
     {
         unit.push_back(Unit(
-            { (float)dis(gen), (float)dis(gen) },    // Position
-            { (float)dis(gen), (float)dis(gen) },    // DirectionView
-            135.5,                                               // AngleView
-            2));                                                   // DistanceView;
+            { (float)dis(gen), (float)dis(gen) },   //Position
+            { (float)dis(gen), (float)dis(gen) },  //DirectionView
+            135.5,  //AngleView
+            2));       //DistanceView));
     }
 
 #pragma endregion
@@ -171,66 +285,40 @@ void AddUnit()
 
 int main()
 {
-    // Stopwatch :)
-    float start_time = clock(); //Start time
+    float start_time = clock(); // начальное время
 
-    // Creates a set of units and saves data about them if the save file is empty, otherwise loads data about units from it
+    //Создаёт набор юнитов и сохраняет данные о них,если файл сохранения пустой.Иначе загружает из него данные о юнитах
     if (filesize("InfoAboutUnits.txt") == 0) {
         AddUnit();
         SaveDataUnits();
+        Setup();
     }
     else
     {
         LoadDataUnits();
+        Setup();
     }
 
-    if (unit.size() < 2000) {
+    //Контроллирует проверку видимости юнитом других юнитов,а так-же разделяет на несколько потоков,если юнитов много
+    //Controller(0, unit.size());
+    if (unit.size() < 5000) {
         thread t1(Controller, 0, unit.size());
         t1.join();
     }
-    else if (unit.size() < 4000) {
-        thread t1(Controller, 0, unit.size() / 2);
-        thread t2(Controller, unit.size() / 2, unit.size());
-
-        t1.join();
-        t2.join();
-    }
-    else if (unit.size() < 6000) {
-        thread t1(Controller, 0, unit.size() / 3);
-        thread t2(Controller, unit.size() / 3, unit.size() / 3 * 2);
-        thread t3(Controller, unit.size() / 3 * 2, unit.size());
-
-        t1.join();
-        t2.join();
-        t3.join();
-    }
-    else if (unit.size() < 8000) {
-        thread t1(Controller, 0, unit.size() / 4);
-        thread t2(Controller, unit.size() / 4, unit.size() / 4 *2);
-        thread t3(Controller, unit.size() / 4 * 2, unit.size() / 4 * 3);
-        thread t4(Controller, unit.size() / 4 * 3, unit.size());
-
-        t1.join();
-        t2.join();
-        t3.join();
-        t4.join();
-    }
     else {
-        thread t1(Controller, 0,                           unit.size() / 5);
-        thread t2(Controller, unit.size() / 5,       unit.size() / 5 * 2);
-        thread t3(Controller, unit.size() / 5 * 2, unit.size() / 5 * 3);
-        thread t4(Controller, unit.size() / 5 * 3, unit.size() / 5 * 4);
-        thread t5(Controller, unit.size() / 5 * 4, unit.size());
+        //Controller(0, unit.size());
+
+        thread t1(Controller, 0, unit.size() / 2);
+        thread t2(Controller, unit.size() / 2, unit.size() / 2 * 2);
 
         t1.join();
         t2.join();
-        t3.join();
-        t4.join();
-        t5.join();
     }
 
-    float end_time = clock(); // End Time
-    float search_time = end_time - start_time; // Search Time
+    std::cout << "Hello World!\n";
+
+    float end_time = clock(); // конечное время
+    float search_time = end_time - start_time; // искомое время
 
     printf("%f", search_time);
 }
